93356b69328fb07e529b51a95a50d5110ad017b7
Diego Eduardo Ferreira Biavati <Diego>
2020-10-18 07:52:57 -0300

    Resolv environs

--- siscomsocesa/settings/heroku.py
+++ siscomsocesa/settings/heroku.py
@@ -2,12 +2,12 @@
 Production Settings for Heroku
 """
 
-import environ
+import environs
 
 # If using in your own project, update the project namespace below
 from siscomsocesa.settings.base import *
 
-env = environ.Env(
+env = environs.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
