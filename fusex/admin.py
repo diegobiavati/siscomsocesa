dda9a7fbf6e28c0341ae584fdf2c11c8a22bc11a
Diego Eduardo Ferreira Biavati <Diego>
2021-02-09 08:33:57 -0300

    Adicionado funcionalidades de cadastro no app fusex

--- fusex/admin.py
+++ fusex/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
-from .models import Beneficiario
+from .models import Beneficiario,Procedimento,OcsPsa,Atendimento
 
 # Register your models here.
 @admin.register(Beneficiario)
 class BeneficiarioAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('nome','preccp','status')
+    search_fields = ('nome','preccp','cpf')
+
+@admin.register(Procedimento)
+class ProcedimentoAdmin(admin.ModelAdmin):
+    list_display = ('nome','codigo','status')
+
+@admin.register(OcsPsa)
+class OcsPsaAdmin(admin.ModelAdmin):
+    list_display = ('nome','telefone','email','status')
+
+
+@admin.register(Atendimento)
+class Atendimento(admin.ModelAdmin):
+    list_display = ('id','data','status')
