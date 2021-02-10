6290e537d522dcc100f01533b8fd37b204c679d1
Diego Eduardo Ferreira Biavati <Diego>
2021-02-09 22:27:50 -0300

    Adicionado funcionalidade de preenchimento automático do SlugField.

--- fusex/admin.py
+++ fusex/admin.py
@@ -7,21 +7,25 @@ class BeneficiarioAdmin(admin.ModelAdmin):
     list_display = ('nome','preccp','status')
     search_fields = ('nome','preccp','cpf')
     list_filter = ('status',)
+    prepopulated_fields = {"slug": ("nome",)}
 
 @admin.register(Procedimento)
 class ProcedimentoAdmin(admin.ModelAdmin):
     list_display = ('nome','codigo','status')
     search_fields = ('nome','codigo')
     list_filter = ('status',)
+    prepopulated_fields = {"slug": ("nome",)}
 
 @admin.register(OcsPsa)
 class OcsPsaAdmin(admin.ModelAdmin):
     list_display = ('nome','telefone','email','status')
     search_fields = ('nome','telefone','email')
     list_filter = ('status',)
+    prepopulated_fields = {"slug": ("nome",)}
 
 @admin.register(Atendimento)
 class Atendimento(admin.ModelAdmin):
     list_display = ('id','data','status')
     search_fields = ('data','id')
     list_filter = ('status',)
+    prepopulated_fields = {"slug": ("beneficiario",)}
