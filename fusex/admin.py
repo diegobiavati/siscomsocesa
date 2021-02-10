30ae5e57962e77022bce796e23c2d0d412adaa79
Diego Eduardo Ferreira Biavati <Diego>
2021-02-09 21:14:10 -0300

    Adiconando novas configurações no app Fusex/admin, tais como pesquisa e filtros

--- fusex/admin.py
+++ fusex/admin.py
@@ -6,16 +6,22 @@ from .models import Beneficiario,Procedimento,OcsPsa,Atendimento
 class BeneficiarioAdmin(admin.ModelAdmin):
     list_display = ('nome','preccp','status')
     search_fields = ('nome','preccp','cpf')
+    list_filter = ('status',)
 
 @admin.register(Procedimento)
 class ProcedimentoAdmin(admin.ModelAdmin):
     list_display = ('nome','codigo','status')
+    search_fields = ('nome','codigo')
+    list_filter = ('status',)
 
 @admin.register(OcsPsa)
 class OcsPsaAdmin(admin.ModelAdmin):
     list_display = ('nome','telefone','email','status')
-
+    search_fields = ('nome','telefone','email')
+    list_filter = ('status',)
 
 @admin.register(Atendimento)
 class Atendimento(admin.ModelAdmin):
     list_display = ('id','data','status')
+    search_fields = ('data','id')
+    list_filter = ('status',)
